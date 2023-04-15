87e4f11a78d1044cb3fb56e01340aef9456619d1
trungle26 <leductrung8520@gmail.com>
2023-04-16 00:43:35 +0700

    xac minh

--- src/components/chats/Chat.jsx
+++ src/components/chats/Chat.jsx
@@ -5,41 +5,49 @@ import { ChatContext } from '../../context/ChatContext'
 import { Timestamp, arrayUnion, doc, getDoc, onSnapshot, serverTimestamp, updateDoc } from 'firebase/firestore'
 import { db, storage } from '../../firebase/config'
 import { AuthContext } from '../../context/AuthContext'
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import { getDownloadURL, ref, uploadBytesResumable, getStorage } from 'firebase/storage'
 import { v4 as uuid } from 'uuid';
 import Camera from '../camera/Camera'
 import Canvas from '../canvas/Canvas'
 import { TransactionContext } from '../../context/TransactionContext'
 import axios from 'axios'
 import { set } from 'lodash'
+import { da } from 'date-fns/locale'
+
+
 
 const Chat = () => {
   const { data } = useContext(ChatContext)
   console.log(data);
   const { currentUser } = useContext(AuthContext)
-  // console.log(currentUser);
+  //alert(currentUser.email);
   const [card, setCard] = useState('')
   const [cardUser, setCardUser] = useState('')
+
+  const [photoRegistered, setPhotoRegistered] = useState(false)
+
   useEffect(() => {
     const getUser = async () => {
-        const res = await getDoc(doc(db, "users", currentUser.uid));
-        console.log('hehe');
-        setCard(res._document.data.value.mapValue.fields.numbercard.mapValue?.fields.cardNumber.stringValue);
+      const res = await getDoc(doc(db, "users", currentUser.uid));
+      console.log(res);
+      console.log('hehe');
+      setPhotoRegistered(res._document.data.value.mapValue.fields.photoAuth.booleanValue)
+      setCard(res._document.data.value.mapValue.fields.numbercard.mapValue?.fields.cardNumber.stringValue);
     }
 
     currentAccount && getUser()
-}, [])
+  }, [])
 
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     const getUser = async () => {
       const res = await getDoc(doc(db, "users", data.user.uid));
       console.log('hehe');
       setCardUser(res._document.data.value.mapValue.fields.numbercard.mapValue?.fields.cardNumber.stringValue);
-  }
+    }
 
-  currentAccount && getUser()
-  },[data])
+    currentAccount && getUser()
+  }, [data])
   const [text, setText] = useState('')
   const [img, setImg] = useState(null)
   const [messages, setMessages] = useState([]);
@@ -50,6 +58,7 @@ const Chat = () => {
   const [confirm, setConfirm] = useState(false)
   const [newImage, setNewImage] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
   const { connectWallet, currentAccount, sendTransaction } = useContext(TransactionContext)
   const [values, setValues] = useState({
     addressTo: '',
@@ -64,10 +73,10 @@ const Chat = () => {
 
     e.preventDefault();
     console.log(values)
-    setValues({...values, addressTo: cardUser})
+    setValues({ ...values, addressTo: cardUser })
     if (!values.addressTo || !values.amount || !values.message) return;
 
-    sendTransaction({values, setIsLoading});
+    sendTransaction({ values, setIsLoading });
 
   }
   const handleCapture = async () => {
@@ -85,18 +94,48 @@ const Chat = () => {
   const handleFaceIO = async () => {
     console.log(newImage);
 
-    // fetch('http://localhost:5000/api/init_face', {
+    fetch('http://localhost:5000/api/init_face', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ image_data: newImage, email: currentUser.email })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.valid === true) {
+          updateDoc(doc(db, "users", currentUser.uid), {
+            "photoAuth": true
+          });
+        }
+      });
+    setPhotoRegistered(!photoRegistered);
+    setImage(null)
+    setLink(!link)
+  }
+
+  const handleVerify = async () => {
     fetch('http://localhost:5000/api/is_image_valid', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ image_data: newImage })
+      body: JSON.stringify({ image_data: newImage, email: currentUser.email })
     })
       .then(response => response.json())
-      .then(data => console.log(data));
-    setImage(null)
-    setLink(!link)
+      .then(data => {
+        console.log(data);
+        if (data.valid === false) {
+          setError('Dang nhap that bai')
+          // setConfirm(true)
+          return
+        } else {
+          setImage(null)
+          setLink(!link)
+
+        }
+      });
+
   }
 
   console.log(newImage);
@@ -229,16 +268,16 @@ const Chat = () => {
       </div>
 
       <div className="chat__bottom">
-          {
-            Result&& 
-            <div className='hoichuyentien'>
-              <p>May muon chuyen tien ak</p>
-              <div className='hoi'>
-                <button type="button" class="btn btn-secondary" onClick={() => setResult(!Result)}>No</button>
-                <button type="button" class="btn btn-primary">Yes</button>
-              </div>
+        {
+          Result &&
+          <div className='hoichuyentien'>
+            <p>May muon chuyen tien ak</p>
+            <div className='hoi'>
+              <button type="button" class="btn btn-secondary" onClick={() => setResult(!Result)}>No</button>
+              <button type="button" class="btn btn-primary">Yes</button>
             </div>
-          }
+          </div>
+        }
         <ul className="chat__bottom-left">
           <li>
             <button onClick={() => setShow(!show)} className='threeButton' type="button" class="btn btn-primary" id="primary" data-toggle="modal" data-target="#exampleModalCenter">
@@ -284,7 +323,7 @@ const Chat = () => {
                                     setLink(!link)
                                     setShow(!show)
                                   }} type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                  <button type="button"  onClick={handleSubmit} class="btn btn-primary" id="upload"
+                                  <button type="button" onClick={handleSubmit} class="btn btn-primary" id="upload"
                                   >Transfer</button>
                                 </div>
                               </>
@@ -347,11 +386,11 @@ const Chat = () => {
                         :
                         <>
                           <div class="modal-body">
-                          {
-                            !currentAccount?<h5 style={{color: 'red'}}>You have not linked your Viettel Money account!</h5>
-                              : <h5 style={{color: 'green'}}>Link your Viettel Money account succussfully!</h5>
-                          }  
-                          
+                            {
+                              !currentAccount ? <h5 style={{ color: 'red' }}>You have not linked your Viettel Money account!</h5>
+                                : <h5 style={{ color: 'green' }}>Link your Viettel Money account succussfully!</h5>
+                            }
+
                             <div className="item">
                               {!currentAccount &&
                                 (
@@ -359,15 +398,26 @@ const Chat = () => {
                                     connectWallet({
                                       currentUser, card
                                     })
-                                    }}>
+                                  }}>
                                     Connect wallet
                                   </button>
                                 )
                               }
-                              <Camera />
-                              <button style={{ width: '160px', height: '40px', border: 'none', borderRadius: '5px', backgroundColor: '#007bff', color: 'white' }} onClick={handleCapture}>Take Register Photo</button>
-                              {image&&<img  src={newImage} alt=''/>}
+                              <Camera show={show} />
+                              {!photoRegistered &&
+                                (
+                                  <button style={{ width: '160px', height: '40px', border: 'none', borderRadius: '5px', backgroundColor: '#007bff', color: 'white' }} onClick={handleCapture}>Take Register Photo</button>
+                                )
+                              }
+                              {photoRegistered &&
+                                (
+                                  <button style={{ width: '160px', height: '40px', border: 'none', borderRadius: '5px', backgroundColor: '#007bff', color: 'white' }} onClick={handleCapture}>Verify Identity</button>
+                                )
+                              }
 
+                              {image && <img src={newImage} alt='' />}
+
+                              {error && <p>{error}</p>}
 
                             </div>
                           </div>
@@ -376,9 +426,22 @@ const Chat = () => {
                               setImage(null)
                               setShow(!show)
                             }} type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" id="upload"
-                              onClick={handleFaceIO}
-                            >Register</button>
+
+                            {image && !photoRegistered &&
+                              (
+                                <button type="button" class="btn btn-primary" id="upload"
+                                  onClick={handleFaceIO}
+                                >Register</button>
+                              )
+                            }
+                            {image && photoRegistered &&
+                              (
+                                <button type="button" class="btn btn-primary" id="upload"
+                                  onClick={() => handleVerify()}
+                                >Verify</button>
+                              )
+                            }
+
                           </div>
                         </>
                     }
